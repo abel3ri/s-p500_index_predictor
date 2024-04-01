617c80435a4905101e5f5b6c0f168a6a7e2534f0
abel3ri <abelmerete@yahoo.com>
2024-04-01 15:28:25 +0300

    init: install and import dependencies

---
+++ main.py
@@ -0,0 +1,6 @@
+import pandas as pd
+# extreme gradient boosting
+# predict outcomes in regression
+# aggregate of mutliple weak prediction models
+import xgboost as xgb
+import matplotlib.pyplot as plt
