4f3f82558798726c3af3d4efcc156a5e1bc2d5fe
abel3ri <abelmerete@yahoo.com>
2024-04-03 15:02:29 +0300

    Add: load csv data for training and testing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
-# extreme gradient boosting
-# predict outcomes in regression
+# stands for extreme gradient boosting
+# predict outcomes in regression fashion
 # aggregate of mutliple weak prediction models
 import xgboost as xgb
 import matplotlib.pyplot as plt
+
+data = pd.read_csv("all_stocks_5yr.csv")
